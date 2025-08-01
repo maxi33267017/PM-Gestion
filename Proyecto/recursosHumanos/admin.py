a36e65819391751a026fb0b2dfc2182412352577
maxi33267017 <caamaniom@gmail.com>
2025-08-01 19:10:59 +0000

    Corregir error en admin de PermisoAusencia: eliminar campo fecha_creacion no editable

--- Proyecto/recursosHumanos/admin.py
+++ Proyecto/recursosHumanos/admin.py
@@ -164,7 +164,7 @@ class PermisoAusenciaAdmin(admin.ModelAdmin):
             'fields': ('estado', 'aprobado_por', 'fecha_aprobacion', 'observaciones_aprobacion')
         }),
         ('Auditoría', {
-            'fields': ('fecha_solicitud', 'fecha_creacion', 'fecha_modificacion'),
+            'fields': ('fecha_solicitud', 'fecha_modificacion'),
             'classes': ('collapse',)
         }),
     )
